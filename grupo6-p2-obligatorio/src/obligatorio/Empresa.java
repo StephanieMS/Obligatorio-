c88167fd3bd5b359499834a67a2a45fc9a542a31
StephanieMS <39383974+StephanieMS@users.noreply.github.com>
2018-06-01 16:10:13 -0300

    arreglos

--- grupo6-p2-obligatorio/src/obligatorio/Empresa.java
+++ grupo6-p2-obligatorio/src/obligatorio/Empresa.java
@@ -4,6 +4,7 @@ public class Empresa {
 	
 	private String nombre;
 	private String ruc;
+	private int cantProductosHabilitados;
 	
 	public Empresa(String nombre, String ruc) {
 		this.nombre = nombre;
--- grupo6-p2-obligatorio/src/obligatorio/Main.java
+++ grupo6-p2-obligatorio/src/obligatorio/Main.java
@@ -21,14 +21,14 @@ public class Main {
 
 		List<String[]> datos = csvReader.readAll();
 		
-		HashTable<Integer, Producto> productos;
+		HashTable<Integer, Producto<Integer>> productos;
 		
 		for(int i = 1 ; i < datos.size() ; i++) {
 			
 			String id = (datos.get(i))[2];
 			int idProd = Integer.parseInt(id); 	// falta verificar que el String se pueda castear(en caso de que el idProd != numero)
 			
-			Producto<Integer> prod = new Producto((datos.get(i))[0], (datos.get(i))[1], idProd, (datos.get(i))[20], 
+			Producto<Integer> prod = new Producto<>((datos.get(i))[0], (datos.get(i))[1], idProd, (datos.get(i))[20], 
 				(datos.get(i))[12], (datos.get(i))[5], (datos.get(i))[23], (datos.get(i))[3], (datos.get(i))[10], (datos.get(i))[13]);
 			
 			productos = new HashCerrado<>(datos.size());
