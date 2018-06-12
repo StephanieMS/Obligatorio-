dd2a5e4fb7d05441273ba3dea3f40054b74bb227
StephanieMS <39383974+StephanieMS@users.noreply.github.com>
2018-06-12 15:39:17 -0300

    Iterator corregido

--- grupo6-p2-tads/src/uy/edu/um/prog2/adt/iterator/MyIterator.java
+++ grupo6-p2-tads/src/uy/edu/um/prog2/adt/iterator/MyIterator.java
@@ -3,30 +3,32 @@ package uy.edu.um.prog2.adt.iterator;
 import java.util.Iterator;
 import uy.edu.um.prog2.adt.hash.HashNode;
 
-public class MyIterator<K , T> implements Iterator<T> {
+public class MyIterator<K,T> implements Iterator<T> {
 
-	private int posicion = 0;
-	private HashNode<K, T>[] vectorHash;
+	protected int posicion = 0;
+	protected HashNode<K, T>[] vectorHash;
 
 	public MyIterator(HashNode<K,T>[] vector) {
 		
-		vectorHash = vector;
+		vectorHash=vector;
 		
-		while(vectorHash[posicion] == null  || vectorHash[posicion].getEstaBorrado()) {
+		while ((vector[posicion] == null || vector[posicion].getEstaBorrado()) && posicion < vector.length - 1) {
 			posicion++;
 		}
-
+		if(posicion==vector.length-1 && (vector[posicion] == null || vector[posicion].getEstaBorrado())) {
+			posicion=0;
+		}
 	}
 
+	
 	public boolean hasNext() {
 		
-		boolean bExit = false;
+		boolean bExit=false;
 		
-		int count =posicion;
-		
-		while(!bExit && count < vectorHash.length) {
+		int count = posicion;
+		 while(!bExit && count < vectorHash.length) {
 			 
-			 if(vectorHash[count] != null && !vectorHash[count].getEstaBorrado()) {
+			 if(vectorHash[count]!=null && !vectorHash[count].getEstaBorrado()) {
 				 bExit = true;
 			 }
 			 count++;
@@ -38,15 +40,17 @@ public class MyIterator<K , T> implements Iterator<T> {
 
 	public T next() {
 		
-		T oExit = vectorHash[posicion].getValor();
-		posicion++;
+		T	oExit=null;
+	
 		
-		while(posicion<vectorHash.length && ( vectorHash[posicion] == null || vectorHash[posicion].getEstaBorrado() )) {
+		while( oExit == null && posicion<vectorHash.length) {
+			
+			if(vectorHash[posicion]!=null && !vectorHash[posicion].getEstaBorrado()) {
+				oExit=vectorHash[posicion].getValor();
+			}
 			posicion++;
+			
 		}
-		
-		oExit= vectorHash[posicion].getValor();
-		
 		return oExit;
 	}
 
